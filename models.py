7b6c86396d52764153b1c9faf651c248a554d31a
Ikuyasu Usui <ikuyasu@macbook.local>
2021-08-16 08:55:27 -0500

    note for github setup

---
+++ models.py
@@ -0,0 +1,15 @@
+# create a database
+# books.db
+# create a model
+# title, author, data publicsed, price
+
+# app.py
+# imoprt models
+# main menu - add, search, analysis, exit, view
+# add books to the database
+# edit books
+# delete books
+# search books
+# data cleaning
+
+# loop runs program
